4d074a6c20d7609a6371e771f00ec3f1ce5734b2
R-Morton <remann12@gmail.com>
2023-01-17 17:54:50 +1100

    Moved functions to diff file

--- src/classes.py
+++ src/classes.py
@@ -8,32 +8,10 @@ class NPC:
         self.agility = agility
         self.luck = luck
         self.health = health + (endurance * 10)
-        self.dodge_chance = agility * 3
+        self.dodge_chance = agility * 4
         self.crit_chance = 5 + (luck * 2)
-    
-
-    def attack(self, attacker):
-        damage = 10 + (attacker.strength)
-        print(f"{self.name} has a {self.dodge_chance}% chance to dodge")
-        print(f"{self.name} has a {self.crit_chance}% chance to criticaly hit")
-        if self.dodge_chance_func() == True:
-            print(f"{self.name} dodged the attack")
-        elif self.crit_chance_func() == True:
-            print("Critical hit!")
-            print(f"{attacker.name} deals {damage * 2} damage to {self.name}")
-            self.health -= damage * 2
-        else:
-            print(f"{attacker.name} deals {damage} damage to {self.name}")
-            self.health -= damage
-    
-    def dodge_chance_func(self):
-        if random.randint(1, 100) <= self.dodge_chance:
-            return True
-    
-    def crit_chance_func(self):
-        if random.randint(1, 100) <= self.crit_chance:
-            return True
-
+        self.block_chance = 15 + (endurance * 3)
+        self.stamina = 100 + (agility * 5)
 
 
 class Player(NPC):
--- src/main.py
+++ src/main.py
@@ -1,22 +1,52 @@
 from classes import *
 import os
+from time import sleep
+import random
 
 player = Player('Rhys', 2, 3, 2, 2, 100)
-npc = NPC("Noob", 2, 1, 1, 1, 50)
+npc = NPC("Noob", 1, 1, 1, 1, 50)
 
 def fight():
-    display_health()
     while True:
-        input("Press something to attack")
-        os.system('clear')
-        NPC.attack(npc, player)
-        display_health()
-        if death_checker() == True:
-            break
-        NPC.attack(player, npc)
-        display_health()
-        if death_checker() == True:
-            break
+        os.system('cls')
+        display_health_stamina()
+        print("1 - Light Attack")
+        print("2 - Heavy Attack")
+        user_input = input("Choose an option")
+        match user_input:
+            case "1":
+                if player.stamina < 15:
+                    print("Not enough Stamina")
+                    sleep(2)
+                    os.system('cls')
+                else:
+                    attack(npc, player, 'light')
+                    if death_checker() == True:
+                        break
+                    attack(player, npc, 'light')
+                    if death_checker() == True:
+                        break
+            case "2":
+                if player.stamina < 40:
+                    print("Not enough Stamina")
+                    sleep(2)
+                    os.system('cls')
+                else:
+                    attack(npc, player, 'heavy')
+                    if death_checker() == True:
+                        break
+                    attack(player, npc, 'light')
+                    if death_checker() == True:
+                        break
+            case "3":
+                defensive_stance(player)
+                if death_checker() == True:
+                    break
+            case "4":
+                normal_stance(player)
+                if death_checker() == True:
+                    break
+
 
 def death_checker():
     if player.health <= 0:
@@ -26,9 +56,69 @@ def death_checker():
         print(f"{npc.name} died")
         return True
 
-def display_health():
-    print(f"{npc} health - {npc.health}")
-    print(f"{player} health - {player.health} \n")
+def display_health_stamina():
+    print(f"{npc.name} health - {npc.health}")
+    print(f"{npc.name} stamina - {npc.stamina}")
+    print(f"{player.name} health - {player.health}")
+    print(f"{player.name} stamina - {player.stamina} \n")
+
+def attack(self, attacker, type):
+    damage = 10 + (attacker.strength)
+    attacker.stamina -= 15
+    if type == 'heavy':
+        damage += (damage * 20/100)
+        attacker.stamina -= 25
+    os.system('clear')
+    print(f"{attacker.name} uses {type} attack on {self.name}")
+    sleep(2)
+    if dodge_chance_func(self) == True:
+        return
+    if block_chance_func(self) == True:
+        return
+    elif crit_chance_func(attacker) == True:
+        print(f"{attacker.name} deals {damage * 2} damage to {self.name}")
+        sleep(2)
+        self.health -= damage * 2
+        return
+    else:
+        print(f"{attacker.name} deals {damage} damage to {self.name}")
+        self.health -= damage
+        sleep(2)
+        return
+
+def defensive_stance(self):
+    self.stamina += 30
+    print(f'{self.name} uses defensive stance')
+    sleep(2)
+    attack(self, npc, 'light', True, False)
+
+def normal_stance(self):
+    self.stamina += 50
+    print(f'{self.name} uses normal stance')
+    sleep(2) 
+    attack(self, npc, 'light', False, True)
+
+
+def dodge_chance_func(self):
+    if random.randint(1, 100) <= self.dodge_chance:
+        print(f"Dodged! Due to {self.name}'s {self.dodge_chance}% chance")
+        sleep(2)
+        os.system('cls')
+        return True
+
+def crit_chance_func(self):
+    if random.randint(1, 100) <= self.crit_chance:
+        print(f"Critical hit! due to {self.name} {self.crit_chance}% chance")
+        sleep(2)
+        os.system('cls')
+        return True
+
+def block_chance_func(self):
+    if random.randint(1, 100) <= self.block_chance:
+        print(f"Blocked! Due to {self.name}'s {self.block_chance}% chance")
+        sleep(2)
+        os.system('cls')
+        return True
 
 fight()
 
